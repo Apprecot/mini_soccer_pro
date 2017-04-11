37fda1cd90959def91222bd5003aa9d4bd11a34c
giorgosTh <gt@anaxoft.com>
2017-04-11 13:31:51 +0300

    no message

---
+++ MiniSoccerPro/MiniSoccerPro/Network/Api.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace MiniSoccerPro.Network
+{
+    public class Api
+    {
+        public IObservable<Post> GetPost(int id)
+        {
+            return Observable.Create<Post>((observer) =>
+            {
+                ApprecotRestService.Instance.GetAsync($"https://jsonplaceholder.typicode.com/posts/{id}")
+                .Subscribe((response) => {
+                    var post = JsonConvert.DeserializeObject<Post>(response.Json);
+                    observer.OnNext(post);
+                    observer.OnCompleted();
+                });
+                return Disposable.Empty;
+            });
+        }
+    }
+
+    public class Post
+    {
+         public int userId { get; set; }
+         public int id { get; set; }
+         public string title { get; set; }
+         public string body { get; set; }
+    }
+}
