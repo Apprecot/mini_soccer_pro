ea1695462eaf3fefe5b38f7990dabe50ab56b4e1
giorgosTh <gt@anaxoft.com>
2017-04-11 16:49:55 +0300

    no message

---
+++ MiniSoccerPro/MiniSoccerPro/Extensions/ObservableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Disposables;
+
+namespace MiniSoccerPro.Extensions
+{
+    public static class ObservableExtesions
+    {
+        public static TDisposable DisposeWith<TDisposable>(this TDisposable observable, CompositeDisposable disposables) where TDisposable : class, IDisposable
+        {
+            if (observable != null)
+                disposables.Add(observable);
+
+            return observable;
+        }
+    }
+}
